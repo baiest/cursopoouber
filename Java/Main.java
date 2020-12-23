9995c079cb69321e117cb8645058f1f0a78d1d5d
baiest <ballesteros.juan@correounivalle.edu.co>
2020-12-23 13:13:27 -0500

    Encapsulamiento y polimorfismo

--- Java/Main.java
+++ Java/Main.java
@@ -1,10 +1,14 @@
 class Main{
     public static void main(String[] args) {
         System.out.println("Hola mundo");
-        Car car = new Car("AMQ123", new Account("Camilo Garcia", "12385"));
-        Car car2 = new Car("KJAK212", new Account("Estefania", "7606"));
+        UberX uberx = new UberX("AMQ123", new Account("Camilo Garcia", "12385"), "Chevrolete", "Sonic");
+        uberx.setPassenger(3);;
+        uberx.printDataCar();
+        
+        UberX uberVan = new UberX("KJAK212", new Account("Estefania", "7606"), "Cevrolete", "Sonic");
+        uberVan.printDataCar();
+        /*Car car2 = new Car("KJAK212", new Account("Estefania", "7606"));
         car.passenger = 4;
-        car.printDataCar();
-        car2.printDataCar();
+        car2.printDataCar();*/
     }
 }
--- Java/UberVan.java
+++ Java/UberVan.java
@@ -4,12 +4,22 @@ import java.util.Map;
 public class UberVan extends Car{
     Map<String, Map<String, Integer>> typeCarAcepted;
     ArrayList<String> seatsMaterial;
+    private Integer passenger;
 
     public UberVan(String license, Account driver,
     Map<String, Map<String, Integer>> typeCarAcepted,
     ArrayList<String> seatsMaterial){
         super(license, driver);
-        this.typeCarAcepted = typeCarAcepted;
-        this.seatsMaterial = seatsMaterial;
-    }    
+        /* this.typeCarAcepted = typeCarAcepted;
+        this.seatsMaterial = seatsMaterial; */
+    }  
+    
+    @Override
+    public void setPassenger(Integer passenger) {
+        if(passenger == 6){
+            this.passenger = passenger;
+        }else{
+            System.out.println("Debe ser mayor a 6");
+        }
+    }
 }
